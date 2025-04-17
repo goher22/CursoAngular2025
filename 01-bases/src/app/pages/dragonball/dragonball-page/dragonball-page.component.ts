df97b4c2dc5c42c7646e5b2271d95d2b59a9c79c
goher <gohercarlos@gmail.com>
2025-04-17 18:47:35 -0500

    01-BASES
    Realizar iteraciones de elementos

---
+++ 01-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
@@ -0,0 +1,23 @@
+import { Component, signal } from '@angular/core';
+
+interface Character {
+  id: number;
+  name: string;
+  power:number
+}
+
+@Component({
+  selector: 'app-dragonball-page',
+  imports: [],
+  templateUrl: './dragonball-page.component.html'
+})
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9000 },
+    { id: 2, name: 'Vegeta', power: 8500 },
+    { id: 3, name: 'Gohan', power: 8000 },
+    { id: 4, name: 'Piccolo', power: 7000 },
+    { id: 5, name: 'Krillin', power: 6000 },
+    { id: 6, name: 'Bulma', power: 500 }
+  ]);
+}
