98b2509a1423ca3ff5c12209cc3d2c32e5cf030e
goher <gohercarlos@gmail.com>
2025-04-17 23:26:34 -0500

    01-BASES
    Agregar personaje

--- 01-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
+++ 01-bases/src/app/pages/dragonball/dragonball-page/dragonball-page.component.ts
@@ -12,6 +12,11 @@ interface Character {
   templateUrl: './dragonball-page.component.html'
 })
 export class DragonballPageComponent {
+
+  name = signal<string>('Gohan');
+  power = signal<number>(100);
+
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9000 },
     { id: 2, name: 'Vegeta', power: 8500 },
@@ -20,4 +25,22 @@ export class DragonballPageComponent {
     { id: 5, name: 'Krillin', power: 6000 },
     { id: 6, name: 'Bulma', power: 500 }
   ]);
+
+
+  addCharacter() {
+    if(!this.name() || !this.power() || this.power() <= 0) return;
+
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power()
+    }
+    this.characters.update((prev) => [...prev, newCharacter]);
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.name.set('');
+    this.power.set(0);
+  }
 }
