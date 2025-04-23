3fbbed71b157ed52ecbb41382ceb1900afd8d83e
goher <gohercarlos@gmail.com>
2025-04-23 00:56:09 -0500

    02-gifs-app
    Angular enviroment
    
    Path Alias

--- 02-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.component.ts
+++ 02-gifs-app/src/app/gifs/components/side-menu-header/side-menu-header.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { environment } from '@env/environment';
 
 @Component({
   selector: 'gifs-side-menu-header',
   templateUrl: './side-menu-header.component.html',
 })
 export class SideMenuHeaderComponent {
+
+  envs = environment
 
 }
