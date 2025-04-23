dab44aa9b9068e9ff309cd3bb32dd3b6c22bd72d
goher <gohercarlos@gmail.com>
2025-04-23 01:43:05 -0500

    02-gifs-app
    input de imagenes

--- 02-gifs-app/src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
+++ 02-gifs-app/src/app/gifs/components/gif-list/gif-list-item/gif-list-item.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'gif-list-item',
   templateUrl: './gif-list-item.component.html',
 })
 export class GifListItemComponent {
-
+  imageUrls = input.required<string>();
 }
--- 02-gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
+++ 02-gifs-app/src/app/gifs/components/gif-list/gif-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
 
 @Component({
@@ -7,5 +7,5 @@ import { GifListItemComponent } from "./gif-list-item/gif-list-item.component";
   imports: [GifListItemComponent],
 })
 export class GifListComponent {
-
+  gifs = input.required<string[]>();
 }
