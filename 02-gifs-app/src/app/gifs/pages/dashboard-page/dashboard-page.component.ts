37796bc1ee3668c3c0595ce5cc8ca660e245387b
goher <gohercarlos@gmail.com>
2025-04-23 00:26:30 -0500

    02-gifs-app
    RouterLink - Desde componentes hijos

--- 02-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
+++ 02-gifs-app/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { SideMenuComponent } from '../../components/side-menu/side-menu.component';
 
 @Component({
   selector: 'app-dashboard-page',
-  imports: [RouterOutlet, SideMenuComponent],
+  imports: [SideMenuComponent],
   templateUrl: './dashboard-page.component.html',
 })
 export default class DashboardPageComponent {
