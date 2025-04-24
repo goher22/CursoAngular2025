e7d3617f5d50932ba1149fc5e3e7d75b05f709be
goher <gohercarlos@gmail.com>
2025-04-24 15:26:09 -0500

    02-gifs-app
    Mostrar Historial de busqueda

--- 02-gifs-app/src/app/gifs/pages/gif-history-page/gif-history-page.component.ts
+++ 02-gifs-app/src/app/gifs/pages/gif-history-page/gif-history-page.component.ts
@@ -1,14 +1,21 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { ActivatedRoute } from '@angular/router';
 import { map } from 'rxjs';
+import { GifsService } from '../../services/gifs.service';
+import { GifListComponent } from "../../components/gif-list/gif-list.component";
 
 @Component({
   selector: 'gif-history',
   templateUrl: './gif-history-page.component.html',
+  imports: [GifListComponent],
 })
 export default class GifHistoryPageComponent {
+  gifsService = inject(GifsService);
+
   query = toSignal(inject(ActivatedRoute).params.pipe(
     map(params => params['query'] || ''))
   );
+
+  gifsByKey = computed(() => this.gifsService.getHistoryGifs(this.query()));
 }
--- 02-gifs-app/src/app/gifs/services/gifs.service.ts
+++ 02-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -60,4 +60,8 @@ export class GifsService {
       })
     );
   }
+
+  getHistoryGifs(query: string): Gif[] {
+    return this.searchHistory()[query.toLocaleLowerCase()] ?? [];
+  }
 }
