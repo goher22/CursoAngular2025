875d0b750c0fcd5689906bbea0a8db16f71c977c
goher <gohercarlos@gmail.com>
2025-04-29 14:52:59 -0500

    03-contry-app
    Layout Components

--- 03-country-app/src/app/country/country.routes.ts
+++ 03-country-app/src/app/country/country.routes.ts
@@ -1,14 +1,23 @@
+import { Routes } from "@angular/router";
+import { CountryLayoutComponent } from "./layouts/country-layout/country-layout.component";
+import { By } from "@angular/platform-browser";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
 
-export const countryRoutes = [
+export const countryRoutes: Routes = [
     {
         path: '',
-        Component: ByCapitalPageComponent,
+        component: CountryLayoutComponent,
+        children: [
+            {
+                path: 'by-capital',
+                component: ByCapitalPageComponent,
+            },
+            {
+                path: '**',
+                redirectTo: 'by-capital'
+            }
+        ]
     },
-    {
-        path: '**',
-        redirectTo: ''
-    }
 ]
 
 export default countryRoutes;
