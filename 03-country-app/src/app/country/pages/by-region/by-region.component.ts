013ac4f361799b068423bd688394d6e9176ce395
goher <gohercarlos@gmail.com>
2025-05-01 13:23:32 -0500

    03-contry-app
    Menu superio e iconos

---
+++ 03-country-app/src/app/country/pages/by-region/by-region.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-region',
+  imports: [],
+  templateUrl: './by-region.component.html',
+})
+export class ByRegionComponent {
+
+}
