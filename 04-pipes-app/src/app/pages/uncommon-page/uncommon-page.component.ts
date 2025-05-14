fc897d819affd83c37836ccd2f2f6fbd2b23bad2
goher <gohercarlos@gmail.com>
2025-05-14 08:19:33 -0500

    04-pipes-app
    AsyncPipe con Observables

--- 04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 04-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
 import { AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { interval, map, tap } from 'rxjs';
 
 const client1 = {
   name: 'Client 1',
@@ -90,5 +91,10 @@ export default class UncommonPageComponent {
       }, 3500);
     }
   );
+
+  myObservableTimer = interval(2000).pipe(
+    map((value) => value + 1),
+    tap((value) => console.log('tap: ', value))
+  )
 
 }
