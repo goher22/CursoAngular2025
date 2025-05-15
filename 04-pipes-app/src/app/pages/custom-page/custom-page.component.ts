1d58d362d4504d3cfea1087e6dc38a173d950c2f
goher <gohercarlos@gmail.com>
2025-05-15 08:12:05 -0500

    04-pipes-app
    Preparacion de proyecto

--- 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { ToggleCasePipe } from '../../pipes/toggle-case.pipe';
+import { heroes } from '../../data/heroes.data';
 
 @Component({
   selector: 'app-custom-page',
@@ -12,4 +13,6 @@ export default class CustomPageComponent {
   name = signal("Carlos Gomez");
   
   uppercase = signal(true);
+
+  heroes = signal(heroes);
 }
