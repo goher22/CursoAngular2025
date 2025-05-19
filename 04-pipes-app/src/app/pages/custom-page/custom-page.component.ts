5b6ecf07f972b6a471d287a9d7845995d71361a7
goher <gohercarlos@gmail.com>
2025-05-19 11:52:08 -0500

    04-pipes-app
    Pipe Personalizado
    
    HeroFilterPipe

--- 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
+++ 04-pipes-app/src/app/pages/custom-page/custom-page.component.ts
@@ -8,7 +8,7 @@ import { TitleCasePipe } from '@angular/common';
 import { HeroCreatorPipe } from '../../pipes/hero-creator.pipe';
 import { HeroSortByPipe } from '../../pipes/hero-sort-by.pipe';
 import { Hero } from '../../interfaces/hero.interface';
-import { HeroFilterPipe } from '../../pipes/herofilter.pipe';
+import { HeroFilterPipe } from '../../pipes/hero-filter.pipe';
 
 @Component({
   selector: 'app-custom-page',
