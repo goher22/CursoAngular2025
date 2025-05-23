ac8a5651ea2964427936063746308363052178cb
goher <gohercarlos@gmail.com>
2025-05-23 01:00:41 -0500

    05-reactive-forms
    Switches, Checks y Radio buttons

--- 05-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ 05-reactive-forms-app/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,11 +1,25 @@
 import { JsonPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-switches-page',
-  imports: [JsonPipe],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './switches-page.component.html'
 })
 export class SwitchesPageComponent {
+  private fb = inject(FormBuilder);
+  formUtil = FormUtils;
 
+  myForm:  FormGroup = this.fb.group({
+      gender: ['M', Validators.required],
+      wantNotification: [true],
+      termAndCondition: [false, Validators.requiredTrue],
+    }
+  );
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
