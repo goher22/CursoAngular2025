27b7a206f8eb3ce5d37f84805ebacbc7bf1dab77
goher <gohercarlos@gmail.com>
2025-05-29 07:18:05 -0500

    Preparacion de ejecticio

---
+++ 05-reactive-forms-app/src/app/country/services/country.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Country } from '../interfaces/country.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+  private baseUrl = "https://restcountries.com/v3.1"
+  http = inject(HttpClient);
+
+  private _regions = [
+    "Africa",
+    "Americas",
+    "Asia",
+    "Europa",
+    "Oceania",
+  ]
+
+  get regions(): string []{
+    return [...this._regions];
+  }
+
+  getCountriesByRegion(region: string): Observable<Country[]> {
+    if(!region) return of([]);
+
+    const url = `${this.baseUrl}/region/${ region }?fields=cca3,name,borders`;
+    return this.http.get<Country[]>(url);
+  }
+
+  getCountryByAlphaCode(alphaCode: string): Observable<Country>{
+    const url = `${this.baseUrl}/alpha/${ alphaCode }?fields=cca3,name,borders`;
+    return this.http.get<Country>(url);
+  }
+
+  geCountryBorderByCodes(borders: string[]){
+    
+  }
+}
