8f6bfe8f42a51028adce3e991410d61bc25f0474
goher <gohercarlos@gmail.com>
2025-05-29 07:36:09 -0500

    05-reactive-forms
    Primer selecto controlado
    
    formulario

--- 05-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
+++ 05-reactive-forms-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Country } from '../../interfaces/country.interface';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
 })
 export class CountryPageComponent {
+  fb = inject(FormBuilder);
+  countryService = inject(CountryService);
 
+  regions = signal<Country[]>([]);
+
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
 }
