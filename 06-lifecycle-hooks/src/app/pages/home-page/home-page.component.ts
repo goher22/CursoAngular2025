679dbd919241b1b62e42eb6278606d8acdebbda6
Carlos Gomez Hernandez <gohercarlos@gmail.com>
2025-06-06 08:18:47 -0500

    06-lifecycle-hooks
    OnChange

--- 06-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
+++ 06-lifecycle-hooks/src/app/pages/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { afterNextRender, afterRender, Component, effect, signal } from '@angular/core';
+import { TitleComponent } from '../../components/title/title.component';
 
 const log = (...messages:string[]) => {
   console.log(
@@ -9,7 +10,7 @@ const log = (...messages:string[]) => {
 
 @Component({
   selector: 'app-home-page',
-  imports: [],
+  imports: [TitleComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
