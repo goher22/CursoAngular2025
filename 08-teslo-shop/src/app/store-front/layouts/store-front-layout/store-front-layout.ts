83f1d63e9f7a25f684e6cf98b18a56d814a6ccaa
Carlos Gomez Hernandez <gohercarlos@gmail.com>
2025-06-13 17:08:54 -0500

    08-teslo-shop
    Paginas y rutas

---
+++ 08-teslo-shop/src/app/store-front/layouts/store-front-layout/store-front-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-store-front-layout',
+  imports: [RouterOutlet],
+  templateUrl: './store-front-layout.html'
+})
+export class StoreFrontLayout {
+
+}
