91dcd5959799a60b2210ee74269749032045a521
Carlos Gomez Hernandez <gohercarlos@gmail.com>
2025-06-16 17:24:53 -0500

    08-teslo-shop
    Variables de entorno y parametrizacion

--- 08-teslo-shop/src/app/products/services/products.service.ts
+++ 08-teslo-shop/src/app/products/services/products.service.ts
@@ -2,12 +2,29 @@ import { HttpClient } from "@angular/common/http";
 import { inject } from "@angular/core";
 import { ProductResponse } from "../interfaces/product.interface";
 import { Observable, tap } from "rxjs";
+import { environment } from "src/environments/environment";
+
+const baseUrl = environment.baseUrl;
+
+interface Options {
+    limit?: number
+    offset?: number
+    gender?: number
+}
 
 export class ProductsServices {
     private http = inject(HttpClient);
 
-    getProducts(): Observable<ProductResponse> {
-        return this.http.get<ProductResponse>('http://localhost:3000/api/products')
+    getProducts(options:Options): Observable<ProductResponse> {
+        const {limit = 9, offset = 0, gender = '' } = options;
+        return this.http.get<ProductResponse>(`${baseUrl}/products`, 
+            {
+                params: {
+                    limit,
+                    offset,
+                    gender
+                },
+            })
             .pipe(
                 tap(resp => console.log(resp))
             )
