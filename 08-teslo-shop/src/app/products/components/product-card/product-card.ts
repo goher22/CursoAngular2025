4a1c9d4e6b64b820b90dce3e450f40d63f5baefa
Carlos Gomez Hernandez <gohercarlos@gmail.com>
2025-06-16 08:07:20 -0500

    08-teslo-shop
    Tarjetas de producto y FadeIn animation

---
+++ 08-teslo-shop/src/app/products/components/product-card/product-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'product-card',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './product-card.html'
+})
+export class ProductCard {
+
+}
