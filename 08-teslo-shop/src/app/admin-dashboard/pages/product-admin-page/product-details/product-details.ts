dfa0e1e56acc7fb4673c594d5913da273d1c76d8
Carlos Gomez Hernandez <gohercarlos@gmail.com>
2025-06-19 17:58:36 -0500

    08-teslo-shop
    Formulario de producto

--- 08-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
+++ 08-teslo-shop/src/app/admin-dashboard/pages/product-admin-page/product-details/product-details.ts
@@ -1,14 +1,44 @@
 import { ProductCarousel } from '@/products/components/product-carousel/product-carousel';
 import { Product } from '@/products/interfaces/product.interface';
-import { Component, input } from '@angular/core';
+import { FormUtils } from '@/utils/form-utils';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'product-details',
-  imports: [ProductCarousel],
+  imports: [ProductCarousel, ReactiveFormsModule],
   templateUrl: './product-details.html'
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit {
   product = input.required<Product>()
 
+  fb = inject(FormBuilder)
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    description: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    price: [0, [Validators.required, Validators.min(0)]],
+    stock: [0, [Validators.required, Validators.min(0)]],
+    sizes: [['']],
+    tags: [''],
+    images: [[]],
+    gender: ['men', [Validators.required, Validators.pattern(/men|women|kid|unisex/)]],
+  })
+
   sizes = ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+
+  ngOnInit(): void {
+    this.setFormValue(this.product());
+  }
+
+  setFormValue(formLike: Partial<Product>){
+    this.productForm.reset(this.product() as any)
+    // this.productForm.patchValue(formLike as any)
+    this.productForm.patchValue({tags: formLike.tags?.join(',')})
+  }
+
+  onSubmit() {
+    console.log(this.productForm.value)
+  }
 }
