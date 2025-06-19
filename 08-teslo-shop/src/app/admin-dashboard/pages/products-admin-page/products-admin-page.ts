c7dd0491da7878742170cb6c6814d757660b7266
Carlos Gomez Hernandez <gohercarlos@gmail.com>
2025-06-19 17:15:47 -0500

    08-teslo-shop
    Mostrar productos paginados

--- 08-teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
+++ 08-teslo-shop/src/app/admin-dashboard/pages/products-admin-page/products-admin-page.ts
@@ -1,11 +1,26 @@
 import { ProductTable } from '@/products/components/product-table/product-table';
-import { Component } from '@angular/core';
+import { ProductsServices } from '@/products/services/products.service';
+import { PaginationService } from '@/shared/components/pagination/pagination.service';
+import { Component, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { Pagination } from "../../../shared/components/pagination/pagination/pagination";
 
 @Component({
   selector: 'app-products-admin-page',
-  imports: [ProductTable],
+  imports: [ProductTable, Pagination],
   templateUrl: './products-admin-page.html'
 })
 export class ProductsAdminPage {
+  productsService = inject(ProductsServices);
+  paginationService = inject(PaginationService);
 
+  productsPerPage = signal(10);
+
+  productsResource = rxResource({
+    params: () => ({page: this.paginationService.currentPage() - 1, limit: this.productsPerPage()}),
+    stream: ({params}) => this.productsService.getProducts({
+      offset: params.page,
+      limit: params.limit
+    })
+  })
 }
